c64accfdefdc86c237e708d0855ba7c98dba9f43
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2023-12-04 02:01:48 -0500

    hashing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include "../include/PasswordManager.h"
+#include "../include/MyApp.h"
+#include "../digestpp/digestpp.hpp"
 
 using namespace std;
+using namespace digestpp;
 
 int main() {
-    PasswordManager manager;
-    manager.checkJsonFile();
-    manager.startup();
+    //PasswordManager manager;
+    /*manager.checkJsonFile();
+    manager.startup();*/
+    /*MyApp app;
+    app.OnInit();*/
+    string password = "password";
+    sha256 hasher;
+    cout << password << " = " << hasher.absorb(password).hexdigest() << endl;
+    // if loginInfo.json getPassword(at username) == hash(masterPassword) -> login
+    // salt with username?
+    // how shoj
 }
